07340bf97c92d97e29dc24109f269fa5fba86fb7
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-04-03 07:22:04 +0200

    feat(insert): add entries for db inputs
    Added form for db input to dashboard.

--- src/pages/api/insert/insert.ts
+++ src/pages/api/insert/insert.ts
@@ -4,13 +4,23 @@ import type { APIRoute } from "astro";
 export const prerender = false;
 
 
-
-export const POST: APIRoute = async ({ cookies, request, redirect }) => {
+export const POST: APIRoute = async ({ request, redirect }) => {
     const formData = await request.formData();
-    const title = formData.get('title')?.toString();
-    console.log(title)
+    const title = formData.get('title');
+    const eventdate = formData.get('eventdate');
+    const eventstart = formData.get('eventstart');
+    const eventend = formData.get('eventend');
+    const descheader = formData.get('descheader');
+    const description = formData.get('description');
     const { error } = await supabase.from('events').insert([
-        { title: title },
+        {
+            title: title,
+            eventdate: eventdate,
+            eventstart: eventstart,
+            eventend: eventend,
+            descheader: descheader,
+            description: description
+         },
     ]).select();
     if (error) {
         return new Response(error.message, { status: 500 });
